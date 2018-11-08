4632d34aa45a460511919db6469103c234b5c19b
Chris Laupama <chrislaupama@live.com>
2018-11-08 15:37:11 +1300

    Moved the components to their own files for individual css styling.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import './header.css'
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+        <header>
+          <nav>
+            <div className="nav-wrapper blue-grey darken-4">
+              <a href="/#" className="brand-logo center">
+                <img src="/images/logo.png" alt="Nest" className='logo'></img>
+              </a>
+            </div>
+          </nav>
+        </header>
+    )
+  }
+}
