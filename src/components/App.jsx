bf8f9f75b41fe495eb17651a71c445b96bb24ba9
Chris <chrislaupama@icloud.com>
2018-11-12 20:32:49 +1300

    added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
-import React from "react"
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 
-import Header from './Header/Header'
-import Main from './Main/Main'
-import Footer from './Footer/Footer'
-import FloatingButton from './FloatingButton/FloatingButton'
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+import FloatingButton from "./FloatingButton/FloatingButton";
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <Main />
-                <Footer />
-                <FloatingButton />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path="/" exact component={Header} />
+          <Route path="/" exact component={Main} />
+          <Route path="/" exact component={Footer} />
+          <Route path="/" exact component={FloatingButton} />
+        </div>
+      </Router>
+    );
+  }
 }
