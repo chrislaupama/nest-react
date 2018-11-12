3b680f1523ed278a8b76bf16a2dd2cba7e4fb32c
Chris <chrislaupama@icloud.com>
2018-11-12 19:09:10 +1300

    fixed floating button to a componentDidMount function

--- src/components/FloatingButton/FloatingButton.jsx
+++ src/components/FloatingButton/FloatingButton.jsx
@@ -1,45 +1,53 @@
-import React from 'react'
-import M from 'materialize-css'
-
+import React from "react";
+import M from "materialize-css";
 
 export default class FloatingButton extends React.Component {
+  componentDidMount() {
+    var elems = document.querySelectorAll(".fixed-action-btn");
+    M.FloatingActionButton.init(elems, {
+      direction: "left",
+      hoverEnabled: false
+    });
+  }
   render() {
     return (
-      <div className='fixed-action-btn'>
-        <a href="/#" className='btn-floating btn-large amber'>
-          <i className='large material-icons blue-grey-text text-darken-4'>add</i>
+      <div className="fixed-action-btn">
+        <a href="/#" className="btn-floating btn-large amber">
+          <i className="large material-icons blue-grey-text text-darken-4">
+            add
+          </i>
         </a>
         <ul>
           <li>
-            <a href="/#" className='btn-floating yellow darken-1'>
-              <i className='material-icons'>perm_contact_calendar</i>
+            <a href="/#" className="btn-floating yellow darken-1">
+              <i className="material-icons">perm_contact_calendar</i>
             </a>
           </li>
           <li>
-            <a href="/#" className='btn-floating green'>
-              <i className='material-icons'>add</i>
+            <a href="/#" className="btn-floating green">
+              <i className="material-icons">add</i>
             </a>
           </li>
           <li>
-            <a href="/#" className='btn-floating blue'>
-              <i className='material-icons'>edit</i>
+            <a href="/#" className="btn-floating blue">
+              <i className="material-icons">edit</i>
             </a>
           </li>
           <li>
-            <a href="/#" className='btn-floating red'>
-              <i className='material-icons'>close</i>
+            <a href="/#" className="btn-floating red">
+              <i className="material-icons">close</i>
             </a>
           </li>
         </ul>
       </div>
-    )
+    );
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn')
-    M.FloatingActionButton.init(elems, {
-      direction: 'left',
-      hoverEnabled: false
-    })
-  })
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.fixed-action-btn')
+//     M.FloatingActionButton.init(elems, {
+//       direction: 'left',
+//       hoverEnabled: false
+//     })
+//   })
