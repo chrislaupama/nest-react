1bc91b806688a41405545a91f4a551bce0931048
Chris <chrislaupama@icloud.com>
2018-11-13 13:56:07 +1300

    copyright now links to login page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Footer extends React.Component {
   render() {
@@ -8,14 +9,17 @@ export default class Footer extends React.Component {
           <div className="col s12">
             <div className="card-panel blue-grey darken-3">
               <span className="grey-text text-lighten-1">
-                NEST is an application developed to help modern families keep the daily, weekly, monthly and annual scheduling of a
-                child's activities simple.
+                NEST is an application developed to help modern families keep
+                the daily, weekly, monthly and annual scheduling of a child's
+                activities simple.
               </span>
             </div>
           </div>
         </div>
         <div className="center footer-copyright">
-          <div className="container">© 2018 Chris Laupama</div>
+          <div className="container">
+            <Link to="/">© 2018 Chris Laupama</Link>
+          </div>
         </div>
       </footer>
     )
