22b5266f06f8f18f31c8f6d9d20e0450be03a8b4
Chris <chrislaupama@icloud.com>
2018-11-14 23:31:55 +1300

    Turned the profile card into a classless component

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default class ProfileCard extends React.Component {
-  constructor(props){
-    super(props)
-    this.firstName = props.firstName
-  }
-  render() {
-    return (
-      <div className="col s12 m6">
-        <div className="card blue-grey darken-2 z-depth-4">
-          <div className="card-content white-text">
-            <span className="card-title amber-text">{this.firstName}</span>
-            <p>
-              This section will contain the current schedule for this child,
-              including the name of the adult who has that child on that day.
-            </p>
-          </div>
-          <div className="card-action">
-            <Link to="/app/profile">
-              <a href="/#/">
-                <i className="material-icons amber-text">calendar_today</i>
-              </a>
-            </Link>
-            <Link to="/add/child">
-              <a href="/#/">
-                <i className="material-icons amber-text">add</i>
-              </a>
-            </Link>
-          </div>
+const ProfileCard = props => {
+  return (
+    <div className="col s12 m6">
+      <div className="card blue-grey darken-2 z-depth-4">
+        <div className="card-content white-text">
+          <span className="card-title amber-text">{props.firstName}</span>
+          <p>
+            This section will contain the current schedule for this child,
+            including the name of the adult who has that child on that day.
+          </p>
+        </div>
+        <div className="card-action">
+          <Link to="/app/profile">
+            <a href="/#/">
+              <i className="material-icons amber-text">calendar_today</i>
+            </a>
+          </Link>
+          <Link to="/add/child">
+            <a href="/#/">
+              <i className="material-icons amber-text">add</i>
+            </a>
+          </Link>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default ProfileCard
