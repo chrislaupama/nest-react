a6bc22b8690aa926d8eb56a2bee10a26ef75514e
Chris <chrislaupama@icloud.com>
2018-11-14 19:31:07 +1300

    removed the card section and created a component for it

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,60 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default class Main extends React.Component {
+  state = {
+    child: [
+      {
+        id: 1,
+        name: Bella
+      },
+      {
+        id: 2,
+        name: Cara
+      }
+    ]
+  }
   render() {
     return (
       <div className="section">
         <div className="row">
-          <div className="col s12 m6">
-            <div className="card blue-grey darken-2 z-depth-4">
-              <div className="card-content white-text">
-                <span className="card-title amber-text">Bella</span>
-                <p>
-                  This section will contain the current schedule for this child,
-                  including the name of the adult who has that child on that
-                  day.
-                </p>
-              </div>
-              <div className="card-action">
-                <Link to="/app/profile">
-                  <a href="/#/">
-                    <i className="material-icons amber-text">calendar_today</i>
-                  </a>
-                </Link>
-                <Link to="/add">
-                  <a href="/#/">
-                    <i className="material-icons amber-text">add</i>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="col s12 m6">
-            <div className="card blue-grey darken-2 z-depth-4">
-              <div className="card-content white-text">
-                <span className="card-title amber-text">Cara</span>
-                <p>
-                  This section will contain the current schedule for this child,
-                  including the name of the adult who has that child on that
-                  day.
-                </p>
-              </div>
-              <div className="card-action">
-                <Link to="/app/profile">
-                  <a href="/#/">
-                    <i className="material-icons amber-text">calendar_today</i>
-                  </a>
-                </Link>
-                <Link to="/add">
-                  <a href="/#/">
-                    <i className="material-icons amber-text">add</i>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <ProfileCard />
         </div>
       </div>
     )
