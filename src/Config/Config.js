cd60cf8c550365342ad3ade57c2495dc2e531e7d
Chris <chrislaupama@icloud.com>
2018-11-14 22:34:10 +1300

    added firebase, commented out for future use

---
+++ src/Config/Config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyDhP7iL_ePdo1nQehSvJqmpjqvDRa6bg3s",
+    authDomain: "nest-react.firebaseapp.com",
+    databaseURL: "https://nest-react.firebaseio.com",
+    projectId: "nest-react",
+    storageBucket: "nest-react.appspot.com",
+    messagingSenderId: "1065464970269"
+  }
