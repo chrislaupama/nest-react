8bc91b504a258921ecc7d19781cb7a07e56497b8
Chris <chrislaupama@icloud.com>
2018-11-14 22:35:35 +1300

    a lot of changes to add child to make it fit within the front of the app

--- src/components/AddChild/AddChild.jsx
+++ src/components/AddChild/AddChild.jsx
@@ -15,72 +15,48 @@ export default class Footer extends React.Component {
 
   submitHandler = e => {
     e.preventDefault()
-
+    this.props.addChild(this.state)
     // Final validation before submitting to server
     console.log('SUBMITTED:', this.state)
   }
 
   render() {
     return (
-      <div className="section">
-        <center>
-          <div className="section" />
-          <h3 className="amber-text">Add Child</h3>
-          <div className="container">
-            <div className="z-depth-1 blue-grey darken-4 row login-box">
-              <form className="col s12" onSubmit={this.submitHandler}>
-                <div className="row">
-                  <div className="col s12" />
-                </div>
-
-                <div className="row">
-                  <div className="input-field col s12">
-                    <input
-                      className="validate amber-text"
-                      type="text"
-                      name="firstName"
-                      value={this.state.firstName}
-                      onChange={this.handleChange}
-                      id="first_name"
-                    />
-                    <label for="first_name">First Name</label>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="input-field col s12">
-                    <input
-                      className="validate amber-text"
-                      type="text"
-                      name="lastName"
-                      value={this.state.lastName}
-                      onChange={this.handleChange}
-                      id="last_name"
-                    />
-                    <label for="last_name">Last Name</label>
-                  </div>
-                  <label>
-                    <span className="blue-grey-text text-darken-1">
-                      <b>Your information will never be shared.</b>
-                    </span>
-                  </label>
-                </div>
-
-                <br />
-                <div className="row">
-                  <button
-                    type="submit"
-                    name="btn_login"
-                    className="col s12 btn btn-large waves-effect amber blue-grey-text text-darken-4"
-                  >
-                    Submit
-                  </button>
-                </div>
-                <div className="section" />
-              </form>
+      <div className="col s12 m6">
+        <form className="col s12" onSubmit={this.submitHandler}>
+          <div className="row">
+            <div className="input-field col s12 input-margin">
+              <input
+                className="validate amber-text"
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+                id="first_name"
+              />
+              <label for="first_name">First Name</label>
             </div>
           </div>
-        </center>
+          <div className="row">
+            <div className="input-field col s12 input-margin">
+              <input
+                className="validate amber-text"
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+                id="last_name"
+              />
+              <label for="last_name">Last Name</label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="col s12 btn waves-effect amber blue-grey-text text-darken-4"
+          >
+            Add Child
+          </button>
+        </form>
       </div>
     )
   }
