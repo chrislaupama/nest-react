7977d038a31fa54eca1b39115fc97bea92072f6f
Chris <chrislaupama@icloud.com>
2018-11-14 19:44:37 +1300

    Got the profile working and recieving/displaying props

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class ProfileCard extends React.Component {
+  constructor(props){
+    super(props)
+    this.firstName = props.firstName
+  }
   render() {
     return (
       <div className="col s12 m6">
         <div className="card blue-grey darken-2 z-depth-4">
           <div className="card-content white-text">
-            <span className="card-title amber-text">Bella</span>
+            <span className="card-title amber-text">{this.firstName}</span>
             <p>
               This section will contain the current schedule for this child,
               including the name of the adult who has that child on that day.
