d71d9b75c628d26c7a732230acb95b3d637e29b2
Chris <chrislaupama@icloud.com>
2018-11-14 20:44:37 +1300

    recreated the form to add a child

--- src/components/AddChild/AddChild.jsx
+++ src/components/AddChild/AddChild.jsx
@@ -18,40 +18,61 @@ export default class Footer extends React.Component {
       <div className="section">
         <center>
           <div className="section" />
-          <h4 className="amber-text">Add Child</h4>
-          <div className="z-depth-1 blue-grey darken-4 row login-box">
-            <div className="container">
-              <div className="row">
-                <form className="col s12">
-                  <div className="row">
-                    <div className="input-field col s6">
-                      <input
-                        id="first_name"
-                        name="firstName"
-                        type="text"
-                        value={this.state.firstName}
-                        onChange={this.handleChange}
-                        className="validate amber-text"
-                      />
-                      <label for="first_name">First Name</label>
-                    </div>
-                    <div className="input-field col s6">
-                      <input
-                        id="last_name"
-                        name="lastName"
-                        type="text"
-                        value={this.state.lastName}
-                        onChange={this.handleChange}
-                        className="validate amber-text"
-                      />
-                      <label for="last_name">Last Name</label>
-                    </div>
+          <h3 className="amber-text">Add Child</h3>
+          <div className="container">
+            <div className="z-depth-1 blue-grey darken-4 row login-box">
+
+              <form className="col s12">
+                <div className="row">
+                  <div className="col s12" />
+                </div>
+
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input
+                      className="validate amber-text"
+                      type="text"
+                      name="firstName"
+                      value={this.state.firstName}
+                      onChange={this.handleChange}
+                      id="first_name"
+                    />
+                    <label for="first_name">First Name</label>
                   </div>
-                  <div className="row">
-                    <button className="submit" />
+                </div>
+
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input
+                      className="validate amber-text"
+                      type="text"
+                      name="lastName"
+                      value={this.state.lastName}
+                      onChange={this.handleChange}
+                      id="last_name"
+                    />
+                    <label for="last_name">Last Name</label>
                   </div>
-                </form>
-              </div>
+                  <label>
+                    <span className="blue-grey-text text-darken-1">
+                      <b>Your information will never be shared.</b>
+                    </span>
+                  </label>
+                </div>
+
+                <br />
+                  <div className="row">
+                      <button
+                        type="submit"
+                        name="btn_login"
+                        className="col s12 btn btn-large waves-effect amber blue-grey-text text-darken-4"
+                      >
+                        Submit
+                      </button>
+                  </div>
+                <div className="section" />
+              </form>
+
             </div>
           </div>
         </center>
