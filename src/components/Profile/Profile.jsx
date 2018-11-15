ebfb14ced48d9395fb42539fe2c3d2a4b595c1ca
Chris <chrislaupama@icloud.com>
2018-11-15 21:35:02 +1300

    wrapped some components in containers for styling

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Profile extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <div className="container">
           <h2 className="center amber-text">Bella's Schedule</h2>
         </div>
