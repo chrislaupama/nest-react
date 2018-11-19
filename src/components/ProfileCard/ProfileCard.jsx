4f1cfd0b84fee677124dcc02ee67ee24ebbf1c94
Chris <chrislaupama@icloud.com>
2018-11-20 12:39:59 +1300

    made the profiles now pop up in a modal

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Modal, Button } from 'react-materialize'
+import Profile from '../Profile/Profile';
 
 const ProfileCard = props => {
   return (
     <div className="col s12 m6">
       <div className="card blue-grey darken-2 z-depth-4">
         <div className="card-content white-text">
-          <span className="card-title amber-text">{props.firstName}'s Schedule</span>
+          <span className="card-title amber-text">
+            {props.firstName}'s Schedule
+          </span>
         </div>
         <div className="card-action">
-          <Link to="/main/profile">
-            <a href="/#/">
-              <i className="material-icons amber-text">calendar_today</i>
-            </a>
-          </Link>
+          <Modal
+            className="blue-grey darken-4"
+            actions={
+              <Button
+                className="blue-grey darken-4"
+                waves="light"
+                modal="close"
+                flat
+              >
+                <span className="amber-text">Close</span>
+              </Button>
+            }
+            trigger={
+              <a href="/#/">
+                <i className="material-icons amber-text">calendar_today</i>
+              </a>
+            }
+          >
+            <Profile firstName={props.firstName} schedules={props.schedules} key={props.id} />
+          </Modal>
           <Link to="/#">
             <a href="/#/">
               <i className="material-icons amber-text">add</i>
