bf58768e7b12dc59b059560c518b2334ade44721
Chris <chrislaupama@icloud.com>
2018-11-20 12:39:44 +1300

    converted the profile to a rfc with a schedule rfc

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,61 +1,15 @@
 import React from 'react'
+import ScheduleList from '../ScheduleList/SchduleList'
 
-export default class Profile extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="container">
-          <h2 className="center amber-text">Bella's Schedule</h2>
-        </div>
+const Profile = props => (
+  <div className="container">
+  <div className="container">
+    <h2 className="center amber-text">Bella's Schedule</h2>
+    {props.schedules.map(sched => {
+        return <ScheduleList date={sched.date} parent={sched.parent} activity={sched.activity} key={sched.id} />
+      })}
+  </div>
+</div>
+)
 
-        <ul className="collection">
-          <li className="collection-item avatar blue-grey darken-2 white-text">
-            <i className="material-icons circle amber lighten-1 blue-grey-text text-darken-3">
-              perm_contact_calendar
-            </i>
-            <span className="title amber-text">25 December 2018</span>
-            <p>
-              <strong>Parent:</strong> Chris
-            </p>
-            <p>
-              <strong>Activity:</strong> Christmas
-            </p>
-            <a href="#!" className="secondary-content">
-              <i className="material-icons">grade</i>
-            </a>
-          </li>
-          <li className="collection-item avatar blue-grey darken-2 white-text">
-            <i className="material-icons circle amber lighten-1 blue-grey-text text-darken-3">
-              perm_contact_calendar
-            </i>
-            <span className="title amber-text">31 December 2018</span>
-            <p>
-              <strong>Parent:</strong> Laura
-            </p>
-            <p>
-              <strong>Activity:</strong> New Years Eve
-            </p>
-            <a href="#!" className="secondary-content">
-              <i className="material-icons">grade</i>
-            </a>
-          </li>
-          <li className="collection-item avatar blue-grey darken-2 white-text">
-            <i className="material-icons circle amber lighten-1 blue-grey-text text-darken-3">
-              perm_contact_calendar
-            </i>
-            <span className="title amber-text">1 January 2019</span>
-            <p>
-              <strong>Parent:</strong> Mischa
-            </p>
-            <p>
-              <strong>Activity:</strong> New Years Day
-            </p>
-            <a href="#!" className="secondary-content">
-              <i className="material-icons">grade</i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    )
-  }
-}
+export default Profile
