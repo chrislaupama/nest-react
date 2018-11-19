459da77f0fc1ea3bbf40c7528c9e680704912a9d
Chris <chrislaupama@icloud.com>
2018-11-20 12:48:50 +1300

    pass props to schedule list and also changed the display name

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,13 +3,20 @@ import ScheduleList from '../ScheduleList/SchduleList'
 
 const Profile = props => (
   <div className="container">
-  <div className="container">
-    <h2 className="center amber-text">Bella's Schedule</h2>
-    {props.schedules.map(sched => {
-        return <ScheduleList date={sched.date} parent={sched.parent} activity={sched.activity} key={sched.id} />
+    <div className="container">
+      <h2 className="center amber-text">{props.firstName}'s Schedule</h2>
+      {props.schedules.map(sched => {
+        return (
+          <ScheduleList
+            date={sched.date}
+            parent={sched.parent}
+            activity={sched.activity}
+            key={sched.id}
+          />
+        )
       })}
+    </div>
   </div>
-</div>
 )
 
 export default Profile
