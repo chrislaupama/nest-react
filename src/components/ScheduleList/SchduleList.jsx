d926eb18c4cf672fbde7faa766ebc8250f732f0f
Chris <chrislaupama@icloud.com>
2018-11-20 13:28:26 +1300

    changed the delete icon to red for now

--- src/components/ScheduleList/SchduleList.jsx
+++ src/components/ScheduleList/SchduleList.jsx
@@ -15,7 +15,7 @@ const ScheduleList = props => (
           <strong>Activity:</strong> {props.activity}
         </p>
         <a href="#!" className="secondary-content">
-          <i className="material-icons orange-text text-accent-4">delete</i>
+          <i className="material-icons red-text">delete</i>
         </a>
       </li>
     </ul>
