f4e1573b89d0332010139c4c03e8d1392b001115
Chris <chrislaupama@icloud.com>
2018-11-20 21:20:23 +1300

    created addsched component and css

---
+++ src/components/AddSched/AddSched.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import './addsched.css'
+
+export default class AddSched extends React.Component {
+  state = {
+    date: '',
+    parent: '',
+    activity: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    this.props.addSched(this.props.id, this.state)
+    // Final validation before submitting to server
+    console.log('SUBMITTED:', this.state)
+    // Clear the state to clear the form
+    this.setState({
+      date: '',
+      parent: '',
+      activity: ''
+    })
+  }
+
+  render() {
+    return (
+      <div className="col s12 m6">
+        <form className="col s12" onSubmit={this.submitHandler}>
+          <div className="row">
+            <div className="input-field col s12 input-margin">
+              <input
+                className="amber-text"
+                type="text"
+                name="date"
+                value={this.state.date}
+                onChange={this.handleChange}
+                id="date"
+              />
+              <label for="date">Select Date</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12 input-margin">
+              <input
+                className="amber-text"
+                type="text"
+                name="parent"
+                value={this.state.parent}
+                onChange={this.handleChange}
+                id="parent"
+              />
+              <label for="parent">Parent Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12 input-margin">
+              <input
+                className="amber-text"
+                type="text"
+                name="activity"
+                value={this.state.activity}
+                onChange={this.handleChange}
+                id="activity"
+              />
+              <label for="activity">Activity</label>
+            </div>
+          </div>
+          <div className="center">
+            <button type="submit" className="btn-floating btn-large amber">
+              <i className="material-icons blue-grey-text text-darken-4">add</i>
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
