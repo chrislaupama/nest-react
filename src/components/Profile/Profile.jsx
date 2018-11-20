63c7efcc5926ec8a2013ca690fa9f792c2fbcac9
Chris <chrislaupama@icloud.com>
2018-11-20 13:09:50 +1300

    removed containers so modal would fill more

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,20 +2,18 @@ import React from 'react'
 import ScheduleList from '../ScheduleList/SchduleList'
 
 const Profile = props => (
-  <div className="container">
-    <div className="container">
-      <h2 className="center amber-text">{props.firstName}'s Schedule</h2>
-      {props.schedules.map(sched => {
-        return (
-          <ScheduleList
-            date={sched.date}
-            parent={sched.parent}
-            activity={sched.activity}
-            key={sched.id}
-          />
-        )
-      })}
-    </div>
+  <div>
+    <h3 className="center amber-text">{props.firstName}'s Schedule</h3>
+    {props.schedules.map(sched => {
+      return (
+        <ScheduleList
+          date={sched.date}
+          parent={sched.parent}
+          activity={sched.activity}
+          key={sched.id}
+        />
+      )
+    })}
   </div>
 )
 
